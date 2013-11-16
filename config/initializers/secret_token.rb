8630e77e0f02b9cf0f39ecf511deaa0f1985fb53
Jordan <eightyfour@gmail.com>
2013-11-16 15:53:50 -0500

    it works...

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AtlDistricts::Application.config.secret_key_base = '2df7f163b394f351bb3f59ddf6243034ad1b2e24d4e7b368ac5c03e29f56c735c859a01eaef5d232056c5168fe676ab6a48b2fae2125a0d998106f699a100ade'
