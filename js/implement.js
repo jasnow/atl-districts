cbb953ded917afcdd120477363286e655ae70974
Jordan <eightyfour@gmail.com>
2013-12-06 01:18:55 -0500

    bug fix

--- js/implement.js
+++ js/implement.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
 
 L.tileLayer('http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png', {
 	maxZoom: 18,
-	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
+	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery Â© <a href="http://cloudmade.com">CloudMade</a>'
 }).addTo(map);
 
 L.geoJson(districts).addTo(map);
